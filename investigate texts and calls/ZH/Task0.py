dc6f96dc9871238d2cd063040e22b0cf9b0fd1c1
橙子 <1106539897@qq.com>
2018-07-15 20:01:38 +0800

    Update Task0.py
    first_text = {texts[0], texts[1], texts[2]}
    print ("First record of texts, {} texts {} at time {}" .format(texts[1], texts[0], texts[2]))
    last_call = {calls[0], calls[1], calls[2], calls[3]}
    print ("Last record of texts, {} calls {} at time {}, lasting {} seconds" .format(calls[1], calls[0], calls[2], calls[3]))

--- investigate texts and calls/ZH/Task0.py
+++ investigate texts and calls/ZH/Task0.py
@@ -19,4 +19,7 @@ with open('calls.csv', 'r') as f:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
-
+first_text = {texts[0], texts[1], texts[2]}
+print ("First record of texts, {} texts {} at time {}" .format(texts[1], texts[0], texts[2]))
+last_call = {calls[0], calls[1], calls[2], calls[3]}
+print ("Last record of texts, {} calls {} at time {}, lasting {} seconds" .format(calls[1], calls[0], calls[2], calls[3]))
