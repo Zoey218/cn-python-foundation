0ecebd94be75336c6a8ddc5e99842f7bfae7bcf2
橙子 <1106539897@qq.com>
2018-07-20 08:10:57 +0800

    Create task0.py

---
+++ investigate texts and calls/ZH/task0.py
@@ -0,0 +1,4 @@
+first_text = {texts[0], texts[1], texts[2]}
+print("First record of texts, {} texts {} at time {}" .format(texts[1], texts[0], texts[2]))
+last_call = {calls[0], calls[1], calls[2], calls[3]}
+print("Last record of texts, {} calls {} at time {}, lasting {} seconds" .format(calls[1], calls[0], calls[2], calls[3]))
